e3b5a2ccd1e976e54edbdad0589edf80f12ac29d
nimysan <234aini@gmail.com>
2023-09-22 16:39:07 +0800

    add sample

--- README.md
+++ README.md
@@ -9,12 +9,21 @@ pip3 install -r requirements.txt
 
 ## 常见错误
 
-###  numpy文件不存在错误
+### numpy文件不存在错误
+
 ```bash
 ImportError: dlopen(/Library/Python/3.9/site-packages/numpy/core/_multiarray_umath.cpython-39-darwin.so, 0x0002): tried: '/Library/Python/3.9/site-packages/numpy/core/_multiarray_umath.cpython-39-darwin.so' (mach-o file, but is an incompatible architecture (have 'arm64', need 'x86_64')), '/System/Volumes/Preboot/Cryptexes/OS/Library/Python/3.9/site-packages/numpy/core/_multiarray_umath.cpython-39-darwin.so' (no such file), '/Library/Python/3.9/site-packages/numpy/core/_multiarray_umath.cpython-39-darwin.so' (mach-o file, but is an incompatible architecture (have 'arm64', need 'x86_64'))
 ```
 
-解决方案： 
+解决方案：
+
 ```bash
 conda install numpy
+```
+
+## 参考资料
+
+```bash
+#体验一个agents工作过程
+python3 sample.py #
 ```
--- chain.py
+++ chain.py
@@ -1,6 +1,7 @@
 from langchain.prompts import PromptTemplate
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI, BaseLLM
 from langchain.chains import LLMChain
+from langchain.tools import tool
 
 prompt_template = """
 假设你是一个运维专家，需要帮助公司团队调查一些事故的根本原因和给出故障排查建议。
@@ -16,3 +17,42 @@ llm_chain = LLMChain(
     prompt=PromptTemplate.from_template(prompt_template)
 )
 # llm_chain("colorful socks")
+
+
+@tool
+def get_word_length(word: str) -> int:
+    """Returns the length of a word."""
+    return len(word)
+
+tools = [get_word_length]
+
+
+## https://zhuanlan.zhihu.com/p/627948474 知乎的一个例子
+class FugeDataSource:
+    def __init__(self, llm: BaseLLM):
+        self.llm = llm
+
+    def find_product_description(self, product_name: str) -> str:
+        """模拟公司产品的数据库"""
+        product_info = {
+            "好快活": "好快活是一个营销人才平台，以社群+公众号+小程序结合的运营模式展开，帮助企业客户连接并匹配充满才华的营销人才。",
+            "Rimix": "Rimix通过采购流程数字化、完备的项目数据存储记录及标准的供应商管理体系，帮助企业实现采购流程, 透明合规可追溯，大幅节约采购成本。Rimix已为包括联合利华，滴滴出行等多家广告主提供服务，平均可为客户节约采购成本30%。",
+            "Bid Agent": "Bid Agent是一款专为中国市场设计的搜索引擎优化管理工具，支持5大搜索引擎。Bid Agent平均为广告主提升18%的投放效果，同时平均提升47%的管理效率。目前已为阳狮广告、GroupM等知名4A公司提供服务与支持。",
+        }
+        return product_info.get(product_name, "没有找到这个产品")
+
+    def find_company_info(self, query: str) -> str:
+        """模拟公司介绍文档数据库，让llm根据抓取信息回答问题"""
+        context = """
+        关于产品："让广告技术美而温暖"是复歌的产品理念。在努力为企业客户创造价值的同时，也希望让使用复歌产品的每个人都能感受到技术的温度。
+        我们关注用户的体验和建议，我们期待我们的产品能够给每个使用者的工作和生活带来正面的改变。
+        我们崇尚技术，用科技的力量使工作变得简单，使生活变得更加美好而优雅，是我们的愿景。
+        企业文化：复歌是一个非常年轻的团队，公司大部分成员是90后。
+        工作上，专业、注重细节、拥抱创新、快速试错。
+        协作中，开放、坦诚、包容、还有一点点举重若轻的幽默感。
+        以上这些都是复歌团队的重要特质。
+        在复歌，每个人可以平等地表达自己的观点和意见，每个人的想法和意愿都会被尊重。
+        如果你有理想，并拥有被理想所驱使的自我驱动力，我们期待你的加入。
+        """
+        prompt = CONTEXT_QA_PROMPT.format(query=query, context=context)
+        return self.llm(prompt)
